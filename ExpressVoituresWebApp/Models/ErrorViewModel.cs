53b4257a0e65d64748c8dd9d019b692d06e1c302
NXTMax <maxenceP25@gmail.com>
2024-07-01 10:43:56 +0200

    Add project files.

---
+++ ExpressVoituresWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpressVoituresWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
